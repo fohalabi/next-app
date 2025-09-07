ef2de50d12dae0542cef92d6286b4d24c0edbd18
fohalabi <ayomideabimbola79@gmail.com>
2025-09-07 07:07:36 +0100

    Add Contact and Users pages with ButtonComponent integration

---
+++ app/contact/button.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+export default function ButtonComponent() {
+    return (
+        <div>
+            <button onClick={() => alert('Hello!')}>
+                click me
+            </button>
+        </div>
+    )
+}
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,15 +1,15 @@
-'use client';
+import ButtonComponent from './button';
 
-export default function Contact() {
-    return (
-        console.log("Is this in the server or client?"),
-
+export default async function Contact() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+    const posts = await response.json()
+    console.log(posts)
+    return (        
         <div>
             <h1>Contact Us</h1>
             <p>If you have any questions, reach out here.</p>
-            <button onClick={() => alert("Hello!")}>
-                click me!
-            </button>
+
+            <ButtonComponent />
         </div>
     )
 }
