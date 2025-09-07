5226a2739c737dceb353f3c5d57f610475fdc9b3
fohalabi <ayomideabimbola79@gmail.com>
2025-09-07 08:08:53 +0100

    configure data fetching

--- app/users/[userId]/page.tsx
+++ app/users/[userId]/page.tsx
@@ -1,3 +1,12 @@
+async function fetchUser(id: string) {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+    if (!res.ok) {
+        return null;
+    }
+    const user = await res.json();
+    return user;
+}
+
 export default async function UserPage({ 
     params,
 }: {
@@ -5,8 +14,15 @@ export default async function UserPage({
 }) {
 
     const { userId } = await params;
+    const user = await fetchUser(userId);
     return (
-
-        <div>{userId}</div>
+        <div>
+            <h1>{user.name}</h1>
+            <p><strong>Email:</strong> {user.email}</p>
+            <p><strong>Phone:</strong> {user.phone}</p>
+            <p><strong>Website:</strong> <a href={`https://${user.website}`} target="_blank" rel="noopener noreferrer">{user.website}</a></p>
+            <p><strong>Company:</strong> {user.company.name}</p>
+            <p><strong>Address:</strong> {user.address.street}, {user.adress.city}</p>
+        </div>
     )
-}
+} 
