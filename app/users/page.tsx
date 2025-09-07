1463e90c3b5de13465d88352c5766c5e219af6bb
fohalabi <ayomideabimbola79@gmail.com>
2025-09-07 07:53:33 +0100

    added param to fetch user data

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,7 +1,18 @@
-export default function Users() {
+export default async function Users() {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const users = await res.json();
+
     return (
         <div>
-            <h1>user page</h1>
+            <h1> Users List </h1>
+
+            <ul>
+                {users.map((user: { id: number; name: string}) => (
+                    <li key={user.id}>
+                        <h2>{user.name}</h2>
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 }
